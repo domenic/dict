4355cf13227868ebb2fb60584c0dd97f91d16288
Domenic Denicola <domenic@domenicdenicola.com>
2012-10-26 15:19:16 -0400

    Tabs to spaces.

--- dict.js
+++ dict.js
@@ -1,54 +1,54 @@
 "use strict";
 
 function mangle(key) {
-	return "~" + key;
+    return "~" + key;
 }
 
 function methods(obj, methodHash) {
-	for (var methodName in methodHash) {
-		Object.defineProperty(obj, methodName, {
-			value: methodHash[methodName],
-			configurable: true,
-			writable: true
-		});
-	}
+    for (var methodName in methodHash) {
+        Object.defineProperty(obj, methodName, {
+            value: methodHash[methodName],
+            configurable: true,
+            writable: true
+        });
+    }
 }
 
 function assertString(key) {
-	if (typeof key !== "string") {
-		throw new TypeError("key must be a string.");
-	}
+    if (typeof key !== "string") {
+        throw new TypeError("key must be a string.");
+    }
 }
 
 module.exports = function (initializer) {
-	var store = Object.create(null);
+    var store = Object.create(null);
 
-	var dict = {};
-	methods(dict, {
-		get: function (key, defaultValue) {
-			assertString(key);
-			var mangled = mangle(key);
-			return mangled in store ? store[mangled] : defaultValue;
-		},
-		set: function (key, value) {
-			assertString(key);
-			store[mangle(key)] = value;
-		},
-		has: function (key) {
-			assertString(key);
-			return mangle(key) in store;
-		},
-		delete: function (key) {
-			assertString(key);
-			delete store[mangle(key)];
-		}
-	});
+    var dict = {};
+    methods(dict, {
+        get: function (key, defaultValue) {
+            assertString(key);
+            var mangled = mangle(key);
+            return mangled in store ? store[mangled] : defaultValue;
+        },
+        set: function (key, value) {
+            assertString(key);
+            store[mangle(key)] = value;
+        },
+        has: function (key) {
+            assertString(key);
+            return mangle(key) in store;
+        },
+        delete: function (key) {
+            assertString(key);
+            delete store[mangle(key)];
+        }
+    });
 
-	if (typeof initializer === "object" && initializer !== null) {
-		Object.keys(initializer).forEach(function (key) {
-			dict.set(key, initializer[key]);
-		});
-	}
+    if (typeof initializer === "object" && initializer !== null) {
+        Object.keys(initializer).forEach(function (key) {
+            dict.set(key, initializer[key]);
+        });
+    }
 
-	return dict;
+    return dict;
 };
